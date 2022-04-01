bb4006e6b6d3dde2887bb47cdd1b9e6d1aedc83e
Abraham Makovetsky <avimak@gmail.com>
2022-04-01 09:02:11 +0300

    return first available wallets - until UI is implemented

--- src/modal/index.ts
+++ src/modal/index.ts
@@ -1,10 +1,13 @@
 import { IStarknetWindowObject } from "../wallet/types";
 import { WalletProvider } from "../discovery/types";
 
-export default function show(
+export default async function show(
     installed: IStarknetWindowObject[],
     discovery: WalletProvider[],
     preAuthorizedWallets: IStarknetWindowObject[]
 ): Promise<IStarknetWindowObject | undefined> {
-    // todo impl UI
+    // todo impl UI -
+    //  1. select installed wallets
+    //  2. wallets discovery list
+    return installed?.[0];
 }
