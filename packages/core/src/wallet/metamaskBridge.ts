aba9d5dbb6d948d3d8ef0bc30c3fd771fa772e2b
NaorYe <naor@starkware.co>
2023-12-20 16:19:10 +0200

    - Wait for metamask (3 retries)

--- packages/core/src/wallet/metamaskBridge.ts
+++ packages/core/src/wallet/metamaskBridge.ts
@@ -1,7 +1,29 @@
 import type { MetaMaskProvider } from '@consensys/get-starknet/dist/type';
 import detectEthereumProvider from '@metamask/detect-provider';
 
-async function hasSnapSupport(provider: any) {
+async function waitForEthereumProvider(options: { timeout?: number, retries?: number } = {}): Promise<MetaMaskProvider | null> {
+  const { timeout = 3000, retries = 0 } = options;
+
+  let provider: MetaMaskProvider | null = null;
+  try {
+    provider = await detectEthereumProvider<MetaMaskProvider>({ timeout });
+  } catch {
+    // Silent error - do nothing
+  }
+
+  if (provider) {
+    return provider;
+  }
+
+  if (retries === 0) {
+    return null;
+  }
+
+  provider = await waitForEthereumProvider({ timeout, retries: retries - 1 })
+  return provider;
+}
+
+async function hasSnapSupport(provider: MetaMaskProvider) {
   try {
     await provider.request({
       method: "wallet_getSnaps",
@@ -17,7 +39,9 @@ async function bootstrapMetamaskBridge() {
     return;
   }
 
-  const provider = await detectEthereumProvider<MetaMaskProvider>()
+  let provider: MetaMaskProvider | null = null;
+
+  provider = await waitForEthereumProvider({ retries: 3 })
   if (!provider) {
     return;
   }
@@ -27,8 +51,8 @@ async function bootstrapMetamaskBridge() {
     return;
   }
 
-  const {MetaMaskSnapWallet} = await import('@consensys/get-starknet')
+  const { MetaMaskSnapWallet } = await import('@consensys/get-starknet')
   window["starknet_metamask"] = new MetaMaskSnapWallet(provider)
 }
 
-export {bootstrapMetamaskBridge}
+export { bootstrapMetamaskBridge }
