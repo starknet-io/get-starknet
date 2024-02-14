3bd7f7c8d74510a9a737e23bf90fd60a7cddfd4f
Dhruv Kelawala <dhruvrk2000@gmail.com>
2024-02-14 14:35:28 +0000

    chore: export more types

--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -29,6 +29,7 @@ export type {
   RpcMessage,
   IsParamsOptional,
   RpcTypeToMessageMap,
+  RequestFnCall,
 } from "./rpcMessage"
 
 export type {
--- packages/core/src/rpcMessage.ts
+++ packages/core/src/rpcMessage.ts
@@ -1,6 +1,7 @@
 export enum StarknetChainId {
   SN_MAIN = "0x534e5f4d41494e",
   SN_GOERLI = "0x534e5f474f45524c49",
+  SN_SEPOLIA = "0x534e5f5345504f4c4941",
 }
 
 export enum Permission {
@@ -318,8 +319,12 @@ export type RpcMessage = {
 export type IsParamsOptional<T extends keyof RpcTypeToMessageMap> =
   undefined extends RpcTypeToMessageMap[T]["params"] ? true : false
 
+export type RequestFnCall<T extends RpcMessage["type"]> = {
+  type: T
+} & (IsParamsOptional<T> extends true
+  ? { params?: RpcTypeToMessageMap[T]["params"] }
+  : { params: RpcTypeToMessageMap[T]["params"] })
+
 export type RequestFn = <T extends RpcMessage["type"]>(
-  call: { type: T } & (IsParamsOptional<T> extends true
-    ? { params?: RpcTypeToMessageMap[T]["params"] }
-    : { params: RpcTypeToMessageMap[T]["params"] }),
+  call: RequestFnCall<T>,
 ) => Promise<RpcTypeToMessageMap[T]["result"]>
