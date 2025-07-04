eea8373edb0a3576b9c52424af4f861e97c22d9e
jaipaljadeja <jadejajaipal5@gmail.com>
2025-07-04 19:26:13 +0530

    modal: add disconnect method in useConnect

--- packages/modal/src/connect.ts
+++ packages/modal/src/connect.ts
@@ -4,24 +4,34 @@ import { useCallback, useState } from "react";
 export type UseConnect = {
   connected?: WalletWithStarknetFeatures;
   connect: (wallet: WalletWithStarknetFeatures) => void;
+  disconnect: () => void;
 };
 
-export function useConnect() {
+export function useConnect(): UseConnect {
   const [connected, setConnected] = useState<
     WalletWithStarknetFeatures | undefined
   >(undefined);
 
   const connect = useCallback(async (wallet: WalletWithStarknetFeatures) => {
-    console.log("HEHEHEHH");
     const result = await wallet.features["standard:connect"].connect({
       silent: false,
     });
-    console.log(result);
-    setConnected(wallet);
+    if (result.accounts.length > 0) {
+      setConnected(wallet);
+    }
   }, []);
+
+  const disconnect = useCallback(async () => {
+    if (!connected) {
+      return;
+    }
+    await connected.features["standard:disconnect"].disconnect();
+    setConnected(undefined);
+  }, [connected]);
 
   return {
     connected,
     connect,
+    disconnect,
   };
 }
